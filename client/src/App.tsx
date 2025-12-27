7227ed9e9e92768b2e07fd7eb40c2029e7616740
famefunny0-oss <famefunny0@gmail.com>
2025-12-27 15:37:24 +0400

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route, Redirect } from "wouter";
+// Version 2.0 - Backend integrationimport { Switch, Route, Redirect } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
